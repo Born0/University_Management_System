5daaf6b93d1fe9be7c00a8a76400b82e50b75708
Md.Sirajuddin Borno <40140598+Born0@users.noreply.github.com>
2020-10-28 21:00:24 +0600

    Php validation

--- Forms/Teacher_Reg.php
+++ Forms/Teacher_Reg.php
@@ -7,28 +7,28 @@
     <body>
         <fieldset style="width:40%" >
             <legend><b>Teacher Registration</b></legend>
-            <form action="../Result.php" metdod='post'  >
+            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" metdod='post'  >
                 <table align=center >
                     <tr>
                         <td>Name </td>
-                        <td> :   <input type="text"  ></td>
+                        <td> :   <input type="text" name="name" ></td>
                     </tr>
                     <tr>
                         <td>Email </td>
-                        <td>:   <input type="email"  ></td>
+                        <td>:   <input type="email" name="email"  ></td>
                     </tr>
                     <tr>
                         <td>
-                            <label for="birtdday">Date of Birth</label>
+                            <label for="birthday">Date of Birth</label>
                         </td>
                         <td> 
-                            :   <input type="date" id="birtdday" name="birtdday">
+                            :   <input type="date" id="birthday" name="birthday">
                         </td>
                     </tr>
                     <tr>
                         <td><label for="gender"> Gender  </label></td>
                         <td>
-                            <input type="radio" id="male" name="gender" value="male">
+                            <input type="radio" id="male" name="gender" value="male" checked >
                             <label for="male">Male</label>
                             <input type="radio" id="female" name="gender" value="female">
                             <label for="female">Female</label>
@@ -52,19 +52,19 @@
                     </tr>
                     <tr>
                         <td>Contact Number  </td>
-                        <td>:   <input type="text"  ></td>
+                        <td>:   <input type="text"  name="contact" ></td>
                     </tr>
                     <tr>
                         <td>Address </td>
-                        <td>:   <input type="text"   ></td>
+                        <td>:   <input type="text"  name="address"  ></td>
                     </tr>
                     <tr>
                         <td>Religion </td>
-                        <td>:   <input type="text"   ></td>
+                        <td>:   <input type="text"  name="religion"  ></td>
                     </tr>
                     <tr>
                         <td>Joining Year </td>
-                        <td>:   <input type="text"   ></td>
+                        <td>:   <input type="text" name="joining_year"   ></td>
                     </tr>
                     <tr>
                         <td><label for="dept"> Department:</label></td>
@@ -80,19 +80,19 @@
                     </tr>
                     <tr>
                         <td>Designation </td>
-                        <td>:   <input type="text"   ></td>
+                        <td>:   <input type="text"  name="designation"  ></td>
                     </tr>
                     <tr>
                         <td>Working Experience </td>
-                        <td>:   <input type="text"   ></td>
+                        <td>:   <input type="text"  name="working_experience" ></td>
                     </tr>
                     <tr>
                         <td>Salary </td>
-                        <td>:   <input type="text"   ></td>
+                        <td>:   <input type="text" name="salary"  ></td>
                     </tr>
                     <tr>
                         <td>Password  </td>
-                        <td>:   <input type="password"  ></td>
+                        <td>:   <input type="password" name="password" ></td>
                     </tr>
                     <tr>
                         <td>
@@ -103,5 +103,86 @@
                 </table>
             </form>
         </fieldset>
+
+
+        <?php
+
+            $name=$email=$dob=$gender=$blood=$contact=$address=$religion=$joining_year=$dept=$designation=$working_experience=$salary=$password="";
+
+            if($_SERVER["REQUEST_METHOD"]=="POST")
+            {
+                $name=$_REQUEST["name"];
+                $email=$_REQUEST["email"];
+                $dob=$_REQUEST["birthday"];
+                $gender=$_REQUEST["gender"];
+                $blood=$_REQUEST["blood"];
+                $contact=$_REQUEST["contact"];
+                $address=$_REQUEST["address"];
+                $religion=$_REQUEST["religion"];
+                $joining_year=$_REQUEST["joining_year"];
+                $dept=$_REQUEST["dept"];
+                $designation=$_REQUEST["designation"];
+                $working_experience=$_REQUEST["working_experience"];
+                $salary=$_REQUEST["salary"];
+                $password=$_REQUEST["password"];
+            }
+
+                echo isEmpty($name);
+                echo isEmpty($email); 
+                echo isEmpty($dob);
+                echo isEmpty($gender);
+                echo isEmpty($blood);
+                echo isEmpty($contact);
+                echo isEmpty($address);
+                echo isEmpty($religion);
+                echo isEmpty($joining_year);
+                echo isEmpty($dept);
+                echo isEmpty($designation);
+                echo isEmpty($working_experience);
+                echo isEmpty($salary);
+                echo isEmpty($password);
+            
+        
+            function isEmpty($attriubte)
+            {
+                if(empty($attriubte)) 
+                {
+                    echo "<br>";
+                    return "please fill the box";   
+                }
+                else 
+                {
+                    echo "<br>";
+                    return $attriubte;
+                }    
+            }
+    
+
+        // echo $dob;
+        // echo "<br>";
+        // echo $gender;
+        // echo "<br>";
+        // echo $blood;
+        // echo "<br>";
+        // echo $contact;
+        // echo "<br>";
+        // echo $address;
+        // echo "<br>";
+        // echo $religion;
+        // echo "<br>";
+        // echo $joining_year;
+        // echo "<br>";
+        // echo $dept;
+        // echo "<br>";
+        // echo $designation;
+        // echo "<br>";
+        // echo $working_experience;
+        // echo "<br>";
+        // echo $salary;
+        // echo "<br>";
+        // echo $password;
+        // echo "<br>";
+
+    ?>
     </body>
 </html>
--- Result.php
+++ Result.php
@@ -8,8 +8,85 @@
  
 </head>
 
-
 <body>
- 
+    <?php
+        $name=$_REQUEST["name"];
+        $email=$_REQUEST["email"];
+        $dob=$_REQUEST["birthday"];
+        $gender=$_REQUEST["gender"];
+        $blood=$_REQUEST["blood"];
+        $contact=$_REQUEST["contact"];
+        $address=$_REQUEST["address"];
+        $religion=$_REQUEST["religion"];
+        $joining_year=$_REQUEST["joining_year"];
+        $dept=$_REQUEST["dept"];
+        $designation=$_REQUEST["designation"];
+        $working_experience=$_REQUEST["working_experience"];
+        $salary=$_REQUEST["salary"];
+        $password=$_REQUEST["password"];
+
+        echo isEmpty($name);
+        echo isEmpty($email); 
+        echo isEmpty($dob);
+        echo isEmpty($gender);
+        echo isEmpty($blood);
+        echo isEmpty($contact);
+        echo isEmpty($address);
+        echo isEmpty($religion);
+        echo isEmpty($joining_year);
+        echo isEmpty($dept);
+        echo isEmpty($designation);
+        echo isEmpty($working_experience);
+        echo isEmpty($salary);
+        echo isEmpty($password);
+
+        
+        function isEmpty($attriubte)
+        {
+            if(empty($attriubte)) 
+            {
+                echo "<br>";
+                return "please fill the box";   
+            }
+            else 
+            {
+                echo "<br>";
+                return $attriubte;
+            }    
+            
+        }
+
+        // echo $dob;
+        // echo "<br>";
+        // echo $gender;
+        // echo "<br>";
+        // echo $blood;
+        // echo "<br>";
+        // echo $contact;
+        // echo "<br>";
+        // echo $address;
+        // echo "<br>";
+        // echo $religion;
+        // echo "<br>";
+        // echo $joining_year;
+        // echo "<br>";
+        // echo $dept;
+        // echo "<br>";
+        // echo $designation;
+        // echo "<br>";
+        // echo $working_experience;
+        // echo "<br>";
+        // echo $salary;
+        // echo "<br>";
+        // echo $password;
+        // echo "<br>";
+
+    ?>
+
+
+
+
+
+
 </body>
 </html>
