6cb48de52f5249a71e497f66bbf9cff356d9b322
Shafiq Ahmed <zero709@users.noreply.github.com>
2020-10-30 17:06:31 +0600

    Update IPerson.php

--- Entity/IPerson.php
+++ Entity/IPerson.php
@@ -24,10 +24,83 @@ class IPerson
     {
         return $this->name;
     }
-
+	
+	function setEmail($email)
+    {
+        $this->email=$email;
+    }
+    function getEmail()
+    {
+        return $this->email;
+    }
+	function setId($id)
+    {
+        $this->id=$id;
+    }
+    function getId()
+    {
+        return $this->id;
+    }
+	function setPassword($password)
+    {
+        $this->password=$password;
+    }
+    function getPassword()
+    {
+        return $this->password;
+    }
+	function setDob($dob)
+    {
+        $this->dob=$dob;
+    }
+    function getDob()
+    {
+        return $this->dob;
+    }
+	function setGender($gender)
+    {
+        $this->gender=$gender;
+    }
+    function getGender()
+    {
+        return $this->gender;
+    }
+	
+	function setContact($contact)
+    {
+        $this->contact=$contact;
+    }
+    function getContact()
+    {
+        return $this->contact;
+    }
+	function setAddress($address)
+    {
+        $this->address=$address;
+    }
+    function getAddress()
+    {
+        return $this->address;
+    }
+	function setReligion($religion)
+    {
+        $this->religion=$religion;
+    }
+    function getReligion()
+    {
+        return $this->religion;
+    }
+	function setBlood($blood)
+    {
+        $this->blood=$blood;
+    }
+    function getBlood()
+    {
+        return $this->blood;
+    }
 
 }
 
 
-
 ?>
+</html>
