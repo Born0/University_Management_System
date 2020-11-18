6e44c81089528b1691fa23fc2e0ea8761d0d3d1c
Md.Sirajuddin Borno <40140598+Born0@users.noreply.github.com>
2020-11-18 12:17:15 +0600

    Update student_form.php

--- Forms/student_form.php
+++ Forms/student_form.php
@@ -1,183 +1,182 @@
 <!DOCTYPE html>
 <html>
-	<body>
-		<?php
-		
-			$studentId=$name=$dob=$gender=$country=$city=$email=$address=$admissionDate=$bloodgroup="";
-			$error="";
-			if($_SERVER["REQUEST_METHOD"]=="POST")
-			{
-			$studentId=$_REQUEST["studentId"];
-			$name=$_REQUEST["name"];
-            $dob=$_REQUEST["DOB"];
-			$gender=$_REQUEST["gender"];
-			$Religion=$_REQUEST["religion"];
-			$country=$_REQUEST["country"];
-			$city=$_REQUEST["city"];
-			$address=$_REQUEST["address"];
-            $admissionDate=$_REQUEST["admissionDate"];
-			$bloodgroup=$_REQUEST["bloodgroup"];
-			$email=$_REQUEST["email"];
-			$password=$_REQUEST["password"];
-				
-			if(formNotFilled())
-				$error="All the information must be provided before submitting";
-			
-			
-			}
-		  function formNotFilled()
-		  {
-			  global $name,$studentId,$dob,$gender,$religion,$country,$city,$address,$admissionDate,$bloodgroup,$email;
-			  if($studentId==""||$name==""||$dob==""||$gender==""||$religion==""||$country==""||$city==""||$address==""||$admissionDate==""||$bloodgroup==""||$email==""||$password=="")
-				  return true;
-			  else return false;
-		  }
-			
-			
-			
-		?>
 
-		<h2 align="center">Student Registration Form</h2>
+<body>
+	<?php
 
-			<form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?> method="post">
+	$studentId = $name = $dob = $gender = $country = $city = $email = $address = $admissionDate = $bloodgroup = "";
+	$error = "";
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$studentId = $_REQUEST["studentId"];
+		$name = $_REQUEST["name"];
+		$dob = $_REQUEST["DOB"];
+		$gender = $_REQUEST["gender"];
+		$Religion = $_REQUEST["religion"];
+		$country = $_REQUEST["country"];
+		$city = $_REQUEST["city"];
+		$address = $_REQUEST["address"];
+		$admissionDate = $_REQUEST["admissionDate"];
+		$bloodgroup = $_REQUEST["bloodgroup"];
+		$email = $_REQUEST["email"];
+		$password = $_REQUEST["password"];
 
-				 <table style="width:20%" align="center">
-					  <tr>
-						<td><label> ID</label></td>
-						<td>: <input type="text" id="studentId" name="studentId" value="<?php 
-						echo $studentId;?>"></td><br>
-					  </tr>
-					  
-					  
-					  
-					  <tr>
-					   <td> <label>Name</label></td>
-					   <td> : <input type="text" id="name" name="name" align="center" value="<?php 
-					   echo $name;?>"><br>
-					</td>
-					  </tr>
-					  
-					  
-					   <tr>
-						<td><label>DOB:</label></td>
-						<td>: <input style="font-size: 1rem" type="date" id="DOB" name="DOB" value="<?php
-						 echo $dob;?>"><br>
-					</td>
-					  </tr>
+		if (formNotFilled())
+			$error = "All the information must be provided before submitting";
+	}
+	function formNotFilled()
+	{
+		global $name, $studentId, $dob, $gender, $religion, $country, $city, $address, $admissionDate, $bloodgroup, $email;
+		if ($studentId == "" || $name == "" || $dob == "" || $gender == "" || $religion == "" || $country == "" || $city == "" || $address == "" || $admissionDate == "" || $bloodgroup == "" || $email == "" || $password == "")
+			return true;
+		else return false;
+	}
 
-					  <tr>
-							<td><label>Gender:</label></td>
-							<td>: 
-								<input type="radio" id="maleRadio" name="gender" value="m" checked>
-								<label>Male</label>
-								<input type="radio" id="femaleRadio" name="gender" value="f" >
-								<label>Female</label><br>
-							</td>
-					  </tr>
 
-					  <tr>
-							<td><label>Religion</label></td>
-							<td>:
-							<select name="religion" id="religion" >
-							<option value="islam" >Islam</option>
-							<option value="hindu" >Hindu</option>
-							<option value="christianity" >Christianity</option>
-							<option value="buddhism" >Buddhism</option>
-							
-						</td>
-						</tr>
 
-					  <tr>
-						<td> <label >Country</label>
-					</td>
-						<td>: 
-							<select name="country" id="country" >
-							<option value="bd" >Bangladesh</option>
-							<option value="kr" >Korea</option>
-							<option value="cn" >Canada</option>
-							<option value="jp" >Japan</option>
-							<option value="ch" >China</option>
-							<option value="in" >India</option>
-							<option value="ne" >Nepal</option>
-							<option value="bh" >Bhutan</option>
-						</td>
-					   </tr>
-						
+	?>
 
-					   <tr>
-						<td> <label >City</label>
-					</td>
-						<td>: 
-							<select name="city" id="city" >
-							<option value="dh" >Dhaka</option>
-							<option value="se" >Seoul</option>
-							<option value="ot" >Ottawa</option>
-							<option value="tk" >Tokyo</option>
-							<option value="bj" >Beijing</option>
-							<option value="mb" >Mumbai</option>
-							<option value="kt" >Kathmandu</option>
-							<option value="tm" >Thimphu</option>
-						</td>
-					   </tr>
-					  
-						<tr>
-							<td> <label>Address:</label></td>
-							<td>: <input type="text" id="address" name="address" value="<?php 
-							echo $address;?>">
-						</td>
-						</tr> 
-					  
-					  
-					  
-						<tr>
-							<td><label>Admission Date:</label></td>
-							<td>: <input style="font-size: 1rem" type="date" id="admissionDate" name="admissionDate" value="<?php 
-							echo $admissionDate;?>">
-						</td>
-					   </tr> 
+	<h2 align="center">Student Registration Form</h2>
 
-					   <tr>
-						<td> <label >Blood Group</label>
-					   </td>
-						<td>: 
-							<select name="bloodgroup" id="bloodgroup" >
-							<option value="a+" >A+(positive)</option>
-							<option value="a-" >A-(negative)</option>
-							<option value="b+" >B+(positive)</option>
-							<option value="b-" >B-(negative)</option>
-							<option value="ab+">AB+(positive)</option>
-							<option value="ab-">AB-(negative)</option>
-							<option value="O+" >O+(positive)</option>
-							<option value="O-" >O-(negative)</option>
-						</td>
-					   </tr>
+	<form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?> method="post">
 
-					   <tr>
-						<td><label>Email:</label></td>
-						<td>: <input type="text" id="email" name="email" value="<?php 
-						echo $email;?>">
-					    </td>
-					  </tr> 
+		<table style="width:20%" align="center">
+			<tr>
+				<td><label> ID</label></td>
+				<td>: <input type="text" id="studentId" name="studentId" value="<?php
+																				echo $studentId; ?>"></td><br>
+			</tr>
 
-					  <tr>
-							<td><label>Password:</label></td>
-							<td>: <input type="password" id="password" name="password" value="<?php 
-							echo $password;?>">
-						</td>
-					   </tr> 
-					  
-					   
-				  
-				</table>
-				
-				<div style="color:Black; text-align:center">
-					<i><?php 
-					echo "$error<br><br>";?></i>
-				</div>
-				<div style="text-align:center"> 
-					<input type="submit" value="Submit" align="center"> 
-				</div>
-	
-		</form> 
-	</body>
+
+
+			<tr>
+				<td> <label>Name</label></td>
+				<td> : <input type="text" id="name" name="name" align="center" value="<?php
+																						echo $name; ?>"><br>
+				</td>
+			</tr>
+
+
+			<tr>
+				<td><label>DOB:</label></td>
+				<td>: <input style="font-size: 1rem" type="date" id="DOB" name="DOB" value="<?php
+																							echo $dob; ?>"><br>
+				</td>
+			</tr>
+
+			<tr>
+				<td><label>Gender:</label></td>
+				<td>:
+					<input type="radio" id="maleRadio" name="gender" value="m" checked>
+					<label>Male</label>
+					<input type="radio" id="femaleRadio" name="gender" value="f">
+					<label>Female</label><br>
+				</td>
+			</tr>
+
+			<tr>
+				<td><label>Religion</label></td>
+				<td>:
+					<select name="religion" id="religion">
+						<option value="islam">Islam</option>
+						<option value="hindu">Hindu</option>
+						<option value="christianity">Christianity</option>
+						<option value="buddhism">Buddhism</option>
+
+				</td>
+			</tr>
+
+			<tr>
+				<td> <label>Country</label>
+				</td>
+				<td>:
+					<select name="country" id="country">
+						<option value="bd">Bangladesh</option>
+						<option value="kr">Korea</option>
+						<option value="cn">Canada</option>
+						<option value="jp">Japan</option>
+						<option value="ch">China</option>
+						<option value="in">India</option>
+						<option value="ne">Nepal</option>
+						<option value="bh">Bhutan</option>
+				</td>
+			</tr>
+
+
+			<tr>
+				<td> <label>City</label>
+				</td>
+				<td>:
+					<select name="city" id="city">
+						<option value="dh">Dhaka</option>
+						<option value="se">Seoul</option>
+						<option value="ot">Ottawa</option>
+						<option value="tk">Tokyo</option>
+						<option value="bj">Beijing</option>
+						<option value="mb">Mumbai</option>
+						<option value="kt">Kathmandu</option>
+						<option value="tm">Thimphu</option>
+				</td>
+			</tr>
+
+			<tr>
+				<td> <label>Address:</label></td>
+				<td>: <input type="text" id="address" name="address" value="<?php
+																			echo $address; ?>">
+				</td>
+			</tr>
+
+
+
+			<tr>
+				<td><label>Admission Date:</label></td>
+				<td>: <input style="font-size: 1rem" type="date" id="admissionDate" name="admissionDate" value="<?php
+																												echo $admissionDate; ?>">
+				</td>
+			</tr>
+
+			<tr>
+				<td> <label>Blood Group</label>
+				</td>
+				<td>:
+					<select name="bloodgroup" id="bloodgroup">
+						<option value="a+">A+(positive)</option>
+						<option value="a-">A-(negative)</option>
+						<option value="b+">B+(positive)</option>
+						<option value="b-">B-(negative)</option>
+						<option value="ab+">AB+(positive)</option>
+						<option value="ab-">AB-(negative)</option>
+						<option value="O+">O+(positive)</option>
+						<option value="O-">O-(negative)</option>
+				</td>
+			</tr>
+
+			<tr>
+				<td><label>Email:</label></td>
+				<td>: <input type="text" id="email" name="email" value="<?php
+																		echo $email; ?>">
+				</td>
+			</tr>
+
+			<tr>
+				<td><label>Password:</label></td>
+				<td>: <input type="password" id="password" name="password" value="<?php
+																					echo $password; ?>">
+				</td>
+			</tr>
+
+
+
+		</table>
+
+		<div style="color:Black; text-align:center">
+			<i><?php
+				echo "$error<br><br>"; ?></i>
+		</div>
+		<div style="text-align:center">
+			<input type="submit" value="Submit" align="center">
+		</div>
+
+	</form>
+</body>
+
 </html>
