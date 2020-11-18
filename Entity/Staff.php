a83b1089ae42c724b5a8631ff9bb1c306e1e68c9
Arifuzzaman-Jamy <56483209+Arifuzzaman-Jamy@users.noreply.github.com>
2020-11-18 12:06:15 +0600

    update

---
+++ Entity/Staff.php
@@ -0,0 +1,29 @@
+<?php
+include("Person.php");
+class Staff extends Person
+{
+   
+    private $joinigYear;
+    private $salary;
+
+
+    function setJoinigYear($joinigYear)
+    {
+        $this->joinigYear = $joinigYear;
+    }
+    function getJoinigYear()
+    {
+        return $this->joinigYear;
+    }
+
+    function setSalary($salary)
+    {
+        $this->salary = $salary;
+    }
+    function getSalary()
+    {
+        return $this->salary;
+    }
+
+}
+?>
