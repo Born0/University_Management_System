85f2e7aefa744ae7dc2c70945769351650a98b8c
Md.Sirajuddin Borno <40140598+Born0@users.noreply.github.com>
2021-01-01 21:48:20 +0600

    Update AdminRepo.php

--- Repository/AdminRepo.php
+++ Repository/AdminRepo.php
@@ -15,23 +15,21 @@ class AdminRepo
     function Insert(Admin $admin)
     {
 
-        $sql = "INSERT INTO Users(name,email,dob,gender,bloodgroup,contact_number,address,religion,join_date,salary,password,image)
-VALUES("."'". $admin->getName() . "','" . $admin->getEmail() . "','" . $admin->getDob() . "',
+        $sql = "INSERT INTO admin(name,email,dob,gender,bloodgroup,contact_number,address,religion,join_date,salary,password,image)
+VALUES(" . "'" . $admin->getName() . "','" . $admin->getEmail() . "','" . $admin->getDob() . "',
         '" . $admin->getGender() . "','" . $admin->getBlood() . "','" . $admin->getContact() . "','" . $admin->getAddress() . "',
-        '" . $admin->getReligion() . "','" . $admin->getJoinigYear() . "','" . $admin->getSalary() . "','"  
-           . $admin->getPassword() ."','" . $admin->getImage() . "')";
+        '" . $admin->getReligion() . "','" . $admin->getJoinigYear() . "','" . $admin->getSalary() . "','"
+            . $admin->getPassword() . "','" . $admin->getImage() . "')";
 
-		
-		
-		$result = $this->db->executeQuery($sql);
-        if ($result > 0) 
-			echo "Insertion Successfull";
-		else echo "Insertion Failed";
-		
-		
-	}
-	
-     /*   $result = $this->db->executeQuery($sql);
+
+
+        $result = $this->db->executeQuery($sql);
+        if ($result > 0)
+            echo "Insertion Successfull";
+        else echo "Insertion Failed";
+    }
+
+    /*   $result = $this->db->executeQuery($sql);
         if ($result > 0) {
             $id = $this->Get($admin)->getId();
             //return $id;
@@ -50,7 +48,7 @@ VALUES("."'". $admin->getName() . "','" . $admin->getEmail() . "','" . $admin->g
     function Get(Admin $admin)
     {
 
-        $sql2 = "SELECT * FROM users WHERE email='" . $admin->getEmail() .  "'";
+        $sql2 = "SELECT * FROM admin WHERE email='" . $admin->getEmail() .  "'";
         $result = $this->db->ReaderQuery($sql2);
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
@@ -72,19 +70,19 @@ VALUES("."'". $admin->getName() . "','" . $admin->getEmail() . "','" . $admin->g
         } else {
             echo "0 result ";
         }
-    } 
-	
-	function InsertLogin($id,$email,$password,$type)
-	{
-		$sql3="INSERT INTO login_type(id,email,password,type) VALUES('".$id."','".$email."','".$password."','".$type."')";
-		$result = $this->db->executeQuery($sql3);
-		if ($result > 0) 
-			echo "Login Insertion Successfull";
-		else echo "Insertion Failed";
-	}
-	
-	
-	/*
+    }
+
+    function InsertLogin($id, $email, $password, $type)
+    {
+        $sql3 = "INSERT INTO login_type(id,email,password,type) VALUES('" . $id . "','" . $email . "','" . $password . "','" . $type . "')";
+        $result = $this->db->executeQuery($sql3);
+        if ($result > 0)
+            echo "Login Insertion Successfull";
+        else echo "Insertion Failed";
+    }
+
+
+    /*
     function Update(Teacher $admin)
     {
         $sql3 = "UPDATE teacher SET  t_name='" . $admin->getName() . "' ,blood_group='" . $admin->getBlood() . "',address='" . $admin->getAddress() . "',
@@ -103,7 +101,4 @@ VALUES("."'". $admin->getName() . "','" . $admin->getEmail() . "','" . $admin->g
     function Delete(Teacher $admin)
     {
     }*/
-
 }
-
-?>
